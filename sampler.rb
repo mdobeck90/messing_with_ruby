de298e25cf6877aab608642ae668b6d801a4567c
mdobeck90 <mdobeck@mail.csuchico.edu>
2015-12-06 21:05:30 -0800

    Added random country generator

--- sampler.rb
+++ sampler.rb
@@ -1,6 +1,15 @@
 load "/home/mdobeck/messing_with_ruby/f_names.rb"
 load "/home/mdobeck/messing_with_ruby/l_names.rb"
+load "/home/mdobeck/messing_with_ruby/countries.rb"
 
+#creates name made up of random first name and random last name
 f_name = f_names.sample
 l_name = l_names.sample
 puts f_name + " " + l_name
+
+#samples random country
+puts country = countries.sample
+
+#randomly picks true or false
+tf = ["true", "false"]
+puts tf.sample
