1a4a80e61e83e092ff2a1819751fd99afa0e05f1
mdobeck90 <mdobeck@mail.csuchico.edu>
2015-12-06 20:56:56 -0800

    Figured out I could just be more specific with the rand function...

--- rng.rb
+++ rng.rb
@@ -4,5 +4,8 @@ puts rand(10)
 #generates numbers 1 or 0
 puts rand(2)
 
+#generates random 5 digit number
+puts rand(00000..99999)
 
+#generates random 5 digit number
 puts (1..5).map{"0123456789".chars.to_a.sample}.join
