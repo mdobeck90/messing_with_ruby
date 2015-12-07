5b4349e4a32643e8cba80c3fa788e40ac7d122ac
mdobeck90 <mdobeck@mail.csuchico.edu>
2015-12-06 22:05:22 -0800

    Modified user generation script to use prebuilt sequence user_id generator

--- user_population_script.sql
+++ user_population_script.sql
@@ -1,15 +1,75 @@
 Insert INTO users
       (user_id, user_name, is_artist, f_name, l_name, country, email)
-      VALUES (18162, 'rFlores',  0, 'Arlene', 'Flores', 'SB|Solomon Islands', 'aflores@email.com');
+      VALUES (users_id_seq.nextval, 'oPaul',  1, 'Rose', 'Paul', 'UY|Uruguay', 'rpaul@email.com');
 Insert INTO users
       (user_id, user_name, is_artist, f_name, l_name, country, email)
-      VALUES (3397, 'aHogan',  0, 'Pablo', 'Hogan', 'TO|Tonga', 'phogan@email.com');
+      VALUES (users_id_seq.nextval, 'aPeterson',  1, 'Danielle', 'Peterson', 'HT|Haiti', 'dpeterson@email.com');
 Insert INTO users
       (user_id, user_name, is_artist, f_name, l_name, country, email)
-      VALUES (52182, 'tFranks',  0, 'Itoe', 'Franks', 'AF|Afghanistan', 'ifranks@email.com');
+      VALUES (users_id_seq.nextval, 'iBurris',  0, 'Miyako', 'Burris', 'SE|Sweden', 'mburris@email.com');
 Insert INTO users
       (user_id, user_name, is_artist, f_name, l_name, country, email)
-      VALUES (60815, 'aGardner',  1, 'Paula', 'Gardner', 'PG|Papua New Guinea', 'pgardner@email.com');
+      VALUES (users_id_seq.nextval, 'mAbbott',  0, 'Emi', 'Abbott', 'KZ|Kazakstan', 'eabbott@email.com');
 Insert INTO users
       (user_id, user_name, is_artist, f_name, l_name, country, email)
-      VALUES (16003, 'iMay',  0, 'Michelle', 'May', 'IQ|Iraq', 'mmay@email.com');
+      VALUES (users_id_seq.nextval, 'aMarshall',  1, 'Rafael', 'Marshall', 'ZW|Zimbabwe', 'rmarshall@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'aFranco',  1, 'Kazuko', 'Franco', 'CM|Cameroon', 'kfranco@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'eVelez',  0, 'Dean', 'Velez', 'NG|Nigeria', 'dvelez@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'tMathews',  0, 'Etsuko', 'Mathews', 'ST|Sao Tome And Principe', 'emathews@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'eMack',  0, 'Henri', 'Mack', 'SV|El Salvador', 'hmack@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'aCase',  1, 'Tammy', 'Case', 'ES|Spain', 'tcase@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'aHolman',  0, 'Paula', 'Holman', 'BS|Bahamas', 'pholman@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'oBritt',  1, 'Bonnie', 'Britt', 'LK|Sri Lanka', 'bbritt@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'mLee',  0, 'Emily', 'Lee', 'ID|Indonesia', 'elee@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'tHudson',  1, 'Itoe', 'Hudson', 'VI|Virgin Islands, U.s.', 'ihudson@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'iLynn',  0, 'Cindy', 'Lynn', 'EC|Ecuador', 'clynn@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'eDale',  0, 'Teresa', 'Dale', 'GA|Gabon', 'tdale@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'aDean',  0, 'Nadine', 'Dean', 'AO|Angola', 'ndean@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'aTerrell',  0, 'Walter', 'Terrell', 'KE|Kenya', 'wterrell@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'aBarnes',  0, 'Nana', 'Barnes', 'CO|Colombia', 'nbarnes@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'iPacheco',  0, 'Virginie', 'Pacheco', 'GT|Guatemala', 'vpacheco@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'eOlsen',  0, 'Teresa', 'Olsen', 'LI|Liechtenstein', 'tolsen@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'rHudson',  1, 'Grace', 'Hudson', 'YE|Yemen', 'ghudson@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'oCooke',  1, 'Tomas', 'Cooke', 'MQ|Martinique', 'tcooke@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'iCervantes',  1, 'Wilma', 'Cervantes', 'GU|Guam', 'wcervantes@email.com');
+Insert INTO users
+      (user_id, user_name, is_artist, f_name, l_name, country, email)
+      VALUES (users_id_seq.nextval, 'eStanton',  0, 'Bertha', 'Stanton', 'BM|Bermuda', 'bstanton@email.com');
--- user_sql_generator.rb
+++ user_sql_generator.rb
@@ -15,10 +15,10 @@ email = f_name[0].downcase + l_name.downcase + "@email.com"
 
 open("user_population_script.sql", 'w') {|file|
     #format textually
-  for i in 1..5
+  for i in 1..25
     file.puts("Insert INTO users
       (user_id, user_name, is_artist, f_name, l_name, country, email)
-      VALUES (#{rand(00000..99999)}, '#{user_name}',  #{rand(2)}, '#{f_name}', '#{l_name}', '#{country}', '#{email}');")
+      VALUES (users_id_seq.nextval, '#{user_name}',  #{rand(2)}, '#{f_name}', '#{l_name}', '#{country}', '#{email}');")
     f_name = f_names.sample
     l_name = l_names.sample
     user_name = f_name[1].downcase + l_name   
@@ -26,24 +26,3 @@ open("user_population_script.sql", 'w') {|file|
     email = f_name[0].downcase + l_name.downcase + "@email.com"
   end
 }
-
-#creates name made up of random first name and random last name
-f_name = f_names.sample
-l_name = l_names.sample
-f_name + " " + l_name
-
-#randomly picks true or false
-tf = ["true", "false"]
-tf.sample
-
-#generates a random number between 1 and 9, not including 10
-puts rand(10)
-
-#generates numbers 1 or 0
-puts rand(2)
-
-#generates random 5 digit number
-puts rand(00000..99999)
-
-#generates random 5 digit number
-puts (1..5).map{"0123456789".chars.to_a.sample}.join
